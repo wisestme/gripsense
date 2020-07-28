69c2f9b8c187031849a834f42031c16ca4253e7a
wisestMe <wyhzest@gmail.com>
2020-07-28 04:52:22 +0100

    add event listener to phone icon

--- js/master.js
+++ js/master.js
@@ -1,13 +1,17 @@
 let hamburgerIcon = document.querySelector('.hamburger_menu');
 let closeHamburgerModal = document.querySelector('.close_modal');
 let hamburgerModal = document.querySelector('.hamburger_modal');
+let phoneModalIcon = document.querySelector('.phone_quicklinks');
 
 hamburgerIcon.addEventListener('click', () => {
-	console.log('Click me again');
 	// Position Modal Off-screen
 	hamburgerModal.style.top = 0;
 })
 
 closeHamburgerModal.addEventListener('click', () => {
 	hamburgerModal.style.top = '-100vh';
+})
+
+phoneModalIcon.addEventListener('click', () => {
+	console.log('Did you call me?')
 })
